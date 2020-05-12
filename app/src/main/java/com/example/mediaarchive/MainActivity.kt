67ba00583d4c8c2c7d7d8f01276fb7658c40d57d
Szymon Kędziora <domastoj@tuta.io>
2020-05-12 16:18:36 +0200

    Fragmenty

--- app/src/main/java/com/example/mediaarchive/MainActivity.kt
+++ app/src/main/java/com/example/mediaarchive/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.mediaarchive
 
+import android.graphics.Movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_main_menu.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val fm:FragmentManager = supportFragmentManager
+        val bookInput = BookInput()
+        val movieInput = MovieInput()
+        val gameInput = GameInput()
+
+        go_to_book.setOnClickListener{
+            fm.beginTransaction().add(R.id.fragment_main, bookInput).addToBackStack(null).commit()
+        }
     }
 }
--- app/src/main/java/com/example/mediaarchive/MovieInput.kt
+++ app/src/main/java/com/example/mediaarchive/MovieInput.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [dataInput.newInstance] factory method to
+ * Use the [movieInput.newInstance] factory method to
  * create an instance of this fragment.
  */
-class dataInput : Fragment() {
+class MovieInput : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class dataInput : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_input, container, false)
+        return inflater.inflate(R.layout.fragment_movie_input, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class dataInput : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment dataInput.
+         * @return A new instance of fragment movieInput.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            dataInput().apply {
+            MovieInput().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
